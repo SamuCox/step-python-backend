114ddded2472058f53d3b735f2ec1251f7892873
youjing <onizudango@gmail.com>
2018-03-22 10:55:18 +0800

    notification sender (incomplete)

--- message_sender.py
+++ message_sender.py
@@ -1,22 +1,26 @@
 import firebase_admin
-from firebase_admin import credentials, messaging
+from firebase_admin import credentials, db
+from stepserver.models import User, Message
+from datetime import date
 
+# Fetch the service account key JSON file contents
 cred = credentials.Certificate('service-account-key.json')
-default_app = firebase_admin.initialize_app(cred)
 
-# The topic name can be optionally prefixed with "/topics/".
-topic = 'all'
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+	'databaseURL': 'https://step-customer.firebaseio.com/'
+	})
 
-# See documentation on defining a message payload.
-message = messaging.Message(
-    notification=messaging.Notification(
-		title="pythonYooooo",
-		body="Notification body"
-	)  ,
-    topic=topic,
-)
+# store_latest_steps
+ref = db.reference('profile')
+userList = ref.get()
 
-# Send a message to the devices subscribed to the provided topic.
-response = messaging.send(message)
-# Response is a message ID string.
-print('Successfully sent message:', response)
+# database
+for key, val in userList.items():
+	msgref = db.reference('profile/{key}/messages')
+	#generate msg and store it to firebase
+	message, created = Message.objects.update_or_create(
+		date=date.today(), defaults={'user_id': key, 'message_id': key})
+	print stepcount
+
+def generateMsg():
