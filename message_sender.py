e6e14dc02d98ffdd7058f6c787a5d8112f0b91ba
youjing <onizudango@gmail.com>
2018-03-28 04:39:06 +0800

    survey questions & options database pre-loaded

--- message_sender.py
+++ message_sender.py
@@ -1,6 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials, db
-from stepserver.models import User, Message
 from datetime import date
 
 # Fetch the service account key JSON file contents
@@ -11,128 +10,156 @@ firebase_admin.initialize_app(cred, {
 	'databaseURL': 'https://step-customer.firebaseio.com/'
 	})
 
-def send_messages(fb_db):
-	# store_latest_steps
-	ref = fb_db.reference('profile')
-	userList = ref.get()
-	# database
-	for key, val in userList.items():
-		msgref = fb_db.reference('profile/'+key+'/messages')
-		print("inside a user")
-		new_msg_ref = msgref.push()
-		new_msg_ref.set({
-			'content': "hehahah",
-			'time': {'date': 15, 'month': 2, 'year': 2018},
-			'hasCompleted': False,
-			'sections': [{
-				'type' : "graph",
-				'startTime' : "2018-02-08",
-				'endTime' : "2018-02-15",
-				'steps': [8201, 7500, 9020, 7502, 6702, 9520, 8640],
-				'dates': [8, 9, 10, 11, 12, 13, 14, 15],
-				'content' : "You walk a lot"
-			}, {
-				'type' : "streak-comparison",
-				'target' : "all",
-				'streakID' : 12,
-				'recommendID' : 15,
-				'percentile' : 75,
-				'startTime' : "2018-02-11",
-				'endTime' : "2018-02-15",
-				'steps' : [7502, 6702, 9520, 8640, 10020],
-				'dates': [11, 12, 13, 14, 15],
-				'streakDescription' : "You are walking slowly",
-				'recommendDescription' : "Try walking faster",
-				'survey' : [{
-					'index' : "0",
-					'type' : "field",
-					'question' : "How do you feel when looking at the comparison?",
-					'isAnswered' : False,
-					'answer' : ""
-				}, {
-					'index' : "0",
-					'type' : "likert",
-					'question' : "To what extent do you feel inferior after seeing comparisons to other users?",
-					'isAnswered' : False,
-					'answer' : ""
-				}, {
-					'index' : "1",
-					'type' : "likert",
-					'question' : "To what extent do you feel encouraged after seeing comparisons to other users?",
-					'isAnswered' : False,
-					'answer' : ""
-				}, {
-					'index' : "0",
-					'type' : "mcq",
-					'question' : "Why didn’t you complete the challenge?",
-					'options' : ["I don't like it", "I am too tired"],
-					'isAnswered' : False,
-					'answer' : ""
-				}]
-			}, {
-				'type' : "stats-comparison",
-				'steps' : 7420,
-				'percentile' : 60,
-				'recommendPercentile' : 80,
-				'recommendSteps' : 8920,
-				'statsDescription' : "You are walking too few",
-				'survey' : [{
-					'index' : 0,
-					'type' : "field",
-					'question' : "How do you feel when looking at the comparison?",
-					'isAnswered' : False,
-					'answer' : ""
-				}, {
-					'index' : 0,
-					'type' : "likert",
-					'question' : "To what extent do you feel inferior after seeing comparisons to other users?",
-					'isAnswered' : False,
-					'answer' : ""
-				}, {
-					'index' : 1,
-					'type' : "likert",
-					'question' : "To what extent do you feel encouraged after seeing comparisons to other users?",
-					'isAnswered' : False,
-					'answer' : ""
-				}, {
-					'index' : 0,
-					'type' : "mcq",
-					'question' : "Why didn’t you complete the challenge?",
-					'options' : ["I don't like it", "I am too tired"],
-					'isAnswered' : False,
-					'answer' : ""
-				}]
-			}, {
-				'type' : "challenge",
-				'hasPicked' : False,
-				'pickedIdx' : 0,
-				'hasCompleted' : False,
-				'options' : [
-				{
-					'title': "Walk 10 min",
-					'category': "leisure",
-					'content' : "Go for a 10 min walk today",
-					'fun' : 5,
-					'difficulty' : 2
-				}, {
-					'title': "Listen to music and run",
-					'category': "leisure",
-					'content' : "Listen to 5 songs and run",
-					'fun' : 5,
-					'difficulty' : 3
-				}, {
-					'title': "Find a friend to walk",
-					'category': "Social",
-					'content' : "Contact your best friend and go for a short walk together!",
-					'fun' : 4,
-					'difficulty' : 2
+class MessageSender:
+
+	def __init__(self, fb_db):
+		self.fb_db = fb_db
+
+	#take in uid, steps(?), date
+	#require control grp info
+	#graph: need no. of dates, content -> [need ctrl-grp to decide how many days to show & content (if streak): pull out the last/current streak; if not, show last WEEK]
+	def generate_message(self):
+		print()
+
+	def generate_section_graph(self):
+		return {
+					'type' : "graph",
+					'startTime' : "2018-02-08",
+					'endTime' : "2018-02-15",
+					'steps': [8201, 7500, 9020, 7502, 6702, 9520, 8640],
+					'dates': [8, 9, 10, 11, 12, 13, 14, 15],
+					'content' : "You walk a lot"
 				}
-				]
-			}
-			]
-		})
 
+	def generate_section_streak_comparison(self):
+		return {
+					'type' : "streak-comparison",
+					'target' : "all",
+					'streakID' : 12,
+					'recommendID' : 15,
+					'percentile' : 75,
+					'startTime' : "2018-02-11",
+					'endTime' : "2018-02-15",
+					'steps' : [7502, 6702, 9520, 8640, 10020],
+					'dates': [11, 12, 13, 14, 15],
+					'streakDescription' : "You are walking slowly",
+					'recommendDescription' : "Try walking faster",
+					'survey' : [{
+						'index' : "0",
+						'type' : "field",
+						'question' : "How do you feel when looking at the comparison?",
+						'isAnswered' : False,
+						'answer' : ""
+					}, {
+						'index' : "0",
+						'type' : "likert",
+						'question' : "To what extent do you feel inferior after seeing comparisons to other users?",
+						'isAnswered' : False,
+						'answer' : ""
+					}, {
+						'index' : "1",
+						'type' : "likert",
+						'question' : "To what extent do you feel encouraged after seeing comparisons to other users?",
+						'isAnswered' : False,
+						'answer' : ""
+					}, {
+						'index' : "0",
+						'type' : "mcq",
+						'question' : "Why didn’t you complete the challenge?",
+						'options' : ["I don't like it", "I am too tired"],
+						'isAnswered' : False,
+						'answer' : ""
+					}]
+				}
+	
+	def generate_section_stats_comparison(self):
+		return {
+					'type' : "stats-comparison",
+					'steps' : 7420,
+					'percentile' : 60,
+					'recommendPercentile' : 80,
+					'recommendSteps' : 8920,
+					'statsDescription' : "You are walking too few",
+					'survey' : [{
+						'index' : 0,
+						'type' : "field",
+						'question' : "How do you feel when looking at the comparison?",
+						'isAnswered' : False,
+						'answer' : ""
+					}, {
+						'index' : 0,
+						'type' : "likert",
+						'question' : "To what extent do you feel inferior after seeing comparisons to other users?",
+						'isAnswered' : False,
+						'answer' : ""
+					}, {
+						'index' : 1,
+						'type' : "likert",
+						'question' : "To what extent do you feel encouraged after seeing comparisons to other users?",
+						'isAnswered' : False,
+						'answer' : ""
+					}, {
+						'index' : 0,
+						'type' : "mcq",
+						'question' : "Why didn’t you complete the challenge?",
+						'options' : ["I don't like it", "I am too tired"],
+						'isAnswered' : False,
+						'answer' : ""
+					}]
+				}
 
+	#todo: check control grp to pick the right comparison
+	def generate_section_comparison():
+		section_streak_comparison = self.generate_section_streak_comparison()
+		section_stats_comparison = self.generate_section_stats_comparison()
+		return section_streak_comparison
+
+	def generate_section_challenge():
+		return {
+					'type' : "challenge",
+					'hasPicked' : False,
+					'pickedIdx' : 0,
+					'hasCompleted' : False,
+					'options' : [
+					{
+						'title': "Walk 10 min",
+						'category': "leisure",
+						'content' : "Go for a 10 min walk today",
+						'fun' : 5,
+						'difficulty' : 2
+					}, {
+						'title': "Listen to music and run",
+						'category': "leisure",
+						'content' : "Listen to 5 songs and run",
+						'fun' : 5,
+						'difficulty' : 3
+					}, {
+						'title': "Find a friend to walk",
+						'category': "Social",
+						'content' : "Contact your best friend and go for a short walk together!",
+						'fun' : 4,
+						'difficulty' : 2
+					}
+
+	def send_messages(self):
+		# store_latest_steps
+		ref = self.fb_db.reference('profile')
+		userList = ref.get()
+		# database
+		for key, val in userList.items():
+			msgref = self.fb_db.reference('profile/'+key+'/messages')
+			print("inside a user")
+			section_graph = self.generate_section_graph()
+			section_comparison = self.generate_section_comparison()
+			section_challenge = self.generate_section_challenge()
+			
+			new_msg_ref = msgref.push()
+			new_msg_ref.set({
+				'content': "hehahah",
+				'time': {'date': 15, 'month': 2, 'year': 2018},
+				'hasCompleted': False,
+				'sections': [section_graph, section_comparison, section_challenge]
+			})
 
 
 	#generate msg and store it to firebase
@@ -140,4 +167,5 @@ def send_messages(fb_db):
 		#date=date.today(), defaults={'user_id': key, 'message_id': key})
 	#print stepcount
 	
-send_messages(db)
+ms = MessageSender(db)
+ms.send_messages()
--- stepserver/models.py
+++ stepserver/models.py
@@ -20,3 +20,37 @@ class Message(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	message_id = models.CharField(max_length=200)
 	date = models.DateField()
+
+class Question(models.Model):
+	section = models.CharField(max_length=200)
+	category = models.CharField(max_length=200)
+	#index inside the section
+	index = models.IntegerField(default=0)
+	content = models.CharField(max_length=10000)
+
+class Option(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	content = models.CharField(max_length=10000)
+
+class Streak(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	streak_id = models.IntegerField(default=0)
+	start_date = models.DateField()
+	end_date = models.DateField()
+	median = models.IntegerField(default=0)
+	n_active_day = models.IntegerField(default=0)
+	n_inactive_day = models.IntegerField(default=0)
+	p_500_5k = models.FloatField(default=0)
+	p_5k_10k = models.FloatField(default=0)
+	p_10k_105k = models.FloatField(default=0)
+	p_105k = models.FloatField(default=0)
+	slope = models.FloatField(default=0)
+	cov = models.FloatField(default=0)
+	autocorrelation = models.FloatField(default=0)
+	trend = models.FloatField(default=0)
+	skewness = models.FloatField(default=0)
+	kurtosis = models.FloatField(default=0)
+	days_since_last_prize = models.IntegerField(default=0)
+	points_till_next_prize = models.IntegerField(default=0)
+
+
