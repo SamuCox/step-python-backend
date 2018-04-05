43eddf6576e831d37a27d107a83be3170f9f8903
youjing <onizudango@gmail.com>
2018-04-05 08:10:37 +0800

    streak fixtures fix

--- stepserver/models.py
+++ stepserver/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import date, datetime
+from django.utils.timezone import localtime, now
 
 # Create your models here.
 
@@ -10,11 +12,11 @@ class User(models.Model):
 	user_id = models.CharField(max_length=200)
 	comparison = models.CharField(max_length=100)
 	context = models.CharField(max_length=100)
-	start_date = models.DateField()
+	start_date = models.DateField(default=localtime(now()).date())
 
 class Stepcount(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	date = models.DateField()
+	date = models.DateField(default=localtime(now()).date())
 	step_count = models.IntegerField(default=0)
 
 class Message(models.Model):
@@ -38,8 +40,8 @@ class Streak(models.Model):
 	streak_id = models.IntegerField(default=0)
 	user_cluster_id = models.IntegerField(default=0)
 	streak_index = models.IntegerField(default=0)
-	start_date = models.DateField()
-	end_date = models.DateField()
+	start_date = models.DateField(localtime(now()).date())
+	end_date = models.DateField(localtime(now()).date())
 	cohort_start_date = models.IntegerField(default=0)
 	cohort_end_date = models.IntegerField(default=0)
 	is_active = models.BooleanField(default=False)
@@ -59,20 +61,6 @@ class Streak(models.Model):
 	days_since_last_prize = models.IntegerField(default=0)
 	points_till_next_prize = models.IntegerField(default=0)
 
-class StreakInfo(models.Model):
-	group = models.ForeignKey(StreakGroupInfo, on_delete=models.CASCADE)
-	description = models.CharField(max_length=1000, blank=True, default="")
-	step_level = models.CharField(max_length=100, blank=True, default="") #moderate, moderate high etc.
-	duration = models.CharField(max_length=100, blank=True, default="")
-	break_length = models.CharField(max_length=100, blank=True, default="")
-	consistency = models.CharField(max_length=100, blank=True, default="")
-	engagement = models.CharField(max_length=100, blank=True, default="")
-	is_target_step_level = models.BooleanField(default=False)
-	is_target_duration = models.BooleanField(default=False)
-	is_target_break = models.BooleanField(default=False)
-	is_target_consistency = models.BooleanField(default=False)
-	is_target_engagement = models.BooleanField(default=False)
-
 class StreakGroupInfo(models.Model):
 	name = models.CharField(max_length=200)
 	color = models.CharField(max_length=100)
@@ -84,6 +72,21 @@ class StreakGroupInfo(models.Model):
 	engagement = models.CharField(max_length=100)
 	recommendation_id_ongoing = models.IntegerField(default=0)
 	recommendation_id_upcoming = models.IntegerField(default=0)
+	is_target_step_level = models.BooleanField(default=False)
+	is_target_duration = models.BooleanField(default=False)
+	is_target_break = models.BooleanField(default=False)
+	is_target_consistency = models.BooleanField(default=False)
+	is_target_engagement = models.BooleanField(default=False)
+	has_bad_prediction = models.BooleanField(default=False)
+
+class StreakInfo(models.Model):
+	group = models.ForeignKey(StreakGroupInfo, on_delete=models.CASCADE)
+	description = models.CharField(max_length=1000, blank=True, default="")
+	step_level = models.CharField(max_length=100, blank=True, default="") #moderate, moderate high etc.
+	duration = models.CharField(max_length=100, blank=True, default="")
+	break_length = models.CharField(max_length=100, blank=True, default="")
+	consistency = models.CharField(max_length=100, blank=True, default="")
+	engagement = models.CharField(max_length=100, blank=True, default="")
 	is_target_step_level = models.BooleanField(default=False)
 	is_target_duration = models.BooleanField(default=False)
 	is_target_break = models.BooleanField(default=False)
