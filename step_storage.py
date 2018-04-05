d97320a98d0b88c3029dd66c5a67812a60003f2a
youjing <onizudango@gmail.com>
2018-04-05 09:52:39 +0800

    streak process bug fix

--- step_storage.py
+++ step_storage.py
@@ -2,7 +2,8 @@ import firebase_admin
 from firebase_admin import credentials, db
 from django.db.models import Max
 from stepserver.models import User, Stepcount, Streak
-from datetime import date, datetime
+from datetime import date
+import datetime
 from django.core.exceptions import ObjectDoesNotExist
 
 # Fetch the service account key JSON file contents
@@ -18,8 +19,9 @@ firebase_admin.initialize_app(cred, {
 # print ref.get()
 class StepStorageManager:
 
-	def __init__(self, fb_db, dt_date, dt_datetime, md_User, md_Stepcount, md_Streak, ex_ObjectDoesNotExist):
+	def __init__(self, db_Max, fb_db, dt_date, dt_datetime, md_User, md_Stepcount, md_Streak, ex_ObjectDoesNotExist):
 		self.fb_db = fb_db
+		self.db_Max = db_Max
 		self.md_User = md_User
 		self.md_Stepcount = md_Stepcount
 		self.md_Streak = md_Streak
@@ -51,6 +53,7 @@ class StepStorageManager:
 			current_date = self.dt_date.today()
 			user = self.md_User.objects.get(user_id=key)
 			self.check_and_add_step(user, current_date)
+			self.store_current_cluster(user)
 
 	def check_and_add_user(self, user_id):
 		try:
@@ -89,8 +92,9 @@ class StepStorageManager:
 		step_count = self.md_Stepcount.objects.get(user=user, date=current_date)
 
 		current_streak_index = 0
+		active_level = 50
 
-		if step_count.step_count > 500:
+		if step_count.step_count > active_level:
 			is_today_active = True
 			has_encountered_active = True
 			#check whether this is the start of the first streak
@@ -104,7 +108,7 @@ class StepStorageManager:
 			try:
 				step_count = self.md_Stepcount.objects.get(user=user, date=current_date)
 				count = step_count.step_count
-				if count > 500:
+				if count > active_level:
 					has_encountered_active = True
 					current_date = current_date - self.dt_datetime.timedelta(1)
 					streak_list.append(count)
@@ -112,9 +116,13 @@ class StepStorageManager:
 					# if already encountered active and then encounter inactive now, end the streak
 					if has_encountered_active:
 						has_encountered_inactive = True
+					else:
+						streak_list.append(0)
+					if current_date == start_date:
+						current_date = current_date - self.dt_datetime.timedelta(1)
 			except self.ex_ObjectDoesNotExist:
-				makeup_stepcount = self.md_Stepcount(user=user, date=current_date, step_count=0)
-    		makeup_stepcount.save()
+				streak, created = self.md_Stepcount.objects.update_or_create(
+					user=user, date=current_date, defaults={'step_count': 0})
     		if has_encountered_active:
     			has_encountered_inactive = True
     		else:
@@ -129,27 +137,28 @@ class StepStorageManager:
 		if streak_list.count == 1:
 			is_streak_start = True
 		
-		if not is_first_streak_start:
-			latest_streak_record = md_Streak.objects.all().aggregate(Max('cohort_end_date'))['cohort_end_date__max']
-			prev_cluster_id = md_Streak.objects.get(cohort_end_date = latest_streak_record).streak_id
+		if has_encountered_active:
+			if not is_first_streak_start:
+				latest_streak_record = self.md_Streak.objects.all().aggregate(self.db_Max('cohort_end_date'))['cohort_end_date__max']
+				prev_cluster_id = self.md_Streak.objects.get(cohort_end_date = latest_streak_record).streak_id
 
-		if is_first_streak_start:
-			current_streak_index = 0
-		elif is_streak_start:
-			current_streak_index = prev_cluster_id + 1
-		else:
-			current_streak_index = prev_cluster_id
+			if is_first_streak_start:
+				current_streak_index = 0
+			elif is_streak_start:
+				current_streak_index = prev_cluster_id + 1
+			else:
+				current_streak_index = prev_cluster_id
 
-		cohort_start_date = streak_start_date - start_date
-		cohort_end_date = today_date - start_date
+			cohort_start_date = (streak_start_date - start_date).days
+			cohort_end_date = (today_date - start_date).days
 
-		#save streak to dbstreak_start_date
-		#todo: change to update/add
-		streak, created = self.md_Streak.objects.update_or_create(
-					user=user, end_date=today_date, defaults={'streak_id': streak_cluster_id, 'streak_index': current_streak_index, 'start_date': streak_start_date, 'cohort_start_date': cohort_start_date, 'cohort_end_date': cohort_end_date, 'is_active': is_today_active})
-
-
+			#save streak to dbstreak_start_date
+			#todo: change to update/add
+			streak, created = self.md_Streak.objects.update_or_create(
+						user=user, end_date=today_date, defaults={'streak_id': streak_cluster_id, 'streak_index': current_streak_index, 'start_date': streak_start_date, 'cohort_start_date': cohort_start_date, 'cohort_end_date': cohort_end_date, 'is_active': is_today_active})
 
 
-ms = StepStorageManager(db, date, datetime, User, Stepcount, Streak, ObjectDoesNotExist)
+
+
+ms = StepStorageManager(Max, db, date, datetime, User, Stepcount, Streak, ObjectDoesNotExist)
 ms.store_all_users_and_steps()
