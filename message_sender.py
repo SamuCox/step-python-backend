b1b4ffd8b16dd5bba7d40c8770cbcee1ac0ebd4d
youjing <onizudango@gmail.com>
2018-04-17 12:28:55 +0800

    seems to be working

--- message_sender.py
+++ message_sender.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, db
 from django.db.models import Q
-from stepserver.models import Question, Option, Stepcount, Streak, Streakinfo, StreakGroupInfo, Userclusterinfo
+from stepserver.models import Question, Option, Stepcount, Streak, StreakInfo, StreakGroupInfo, UserClusterInfo, UserClusterGroupInfo
 from datetime import date
 import datetime
 import math
@@ -16,12 +16,17 @@ firebase_admin.initialize_app(cred, {
 
 class MessageSender:
 
-	def __init__(self, fb_db, md_Question, md_Option, md_Stepcount, md_Streak, dt_date, dt_datetime, mt_math):
+	def __init__(self, fb_db, dj_Q, md_Question, md_Option, md_Stepcount, md_Streak, md_Streakinfo, md_Streakgroupinfo, md_Userclusterinfo, md_Userclustergroupinfo, dt_date, dt_datetime, mt_math):
 		self.fb_db = fb_db
+		self.dj_Q = dj_Q
 		self.md_Question = md_Question
 		self.md_Option = md_Option
 		self.md_Stepcount = md_Stepcount
 		self.md_Streak = md_Streak
+		self.md_Streakinfo = md_Streakinfo
+		self.md_Streakgroupinfo = md_Streakgroupinfo
+		self.md_Userclusterinfo = md_Userclusterinfo
+		self.md_Userclustergroupinfo = md_Userclustergroupinfo
 		self.dt_date = dt_date
 		self.dt_datetime = dt_datetime
 		self.mt_math = mt_math
@@ -36,20 +41,23 @@ class MessageSender:
 	#return an array of median steps
 
 	def generate_comparison_calendar(self, streak_cluster_group_id, user_cluster_group_id, start_date, end_date, target, awareness):
-		day = datetime.timedelta(days=1)
+		day = self.dt_datetime.timedelta(days=1)
 		median_step_list = []
 		single_date = start_date
 
-		all_streak_clusters = self.md_Streakinfo.filter(group_id="streak_cluster_group_id")
-		all_user_clusters = self.md_Userclusterinfo.filter(group_id="user_cluster_group_id")
-		
-		q_streak = Q(pk=None)
-		q_user = Q(pk=None)
+		all_streak_clusters = self.md_Streakinfo.objects.filter(group_id=streak_cluster_group_id)
+		all_user_clusters = self.md_Userclusterinfo.objects.filter(group_id=user_cluster_group_id)
+		print("user cluster id: " + str(user_cluster_group_id))
+
+		q_streak = self.dj_Q(pk=None)
+		q_user = self.dj_Q(pk=None)
 		# adding up all individuals under the group
 		for streak_cluster in all_streak_clusters:
-			q_streak = q_streak | Q(streak_cluster_id=streak_cluster.id)
+			print("streak cluster id: " + str(streak_cluster.id))
+			q_streak = q_streak | self.dj_Q(streak_cluster_id=streak_cluster.id)
 		for user_cluster in all_user_clusters:
-			q_user = q_streak | Q(user_cluster_id=user_cluster.id)
+			print("user cluster id: " + str(user_cluster.id))
+			q_user = q_streak | self.dj_Q(user_cluster_id=user_cluster.id)
 
 		while single_date <= end_date:
 			if target == "group" and awareness == "streak":
@@ -63,18 +71,41 @@ class MessageSender:
 
 			count = all_streaks.count()
 			median = all_streaks[int(self.mt_math.floor(count/2))]
-			median_step_list.append(median)
+			median_step_list.append(median.step_count)
 			single_date = single_date + day	
 		return median_step_list
 
-	def generate_comparison_group_cohort(self, streak_id, group_id, start_date, end_date, target, awareness):
+	def generate_comparison_cohort(self, streak_cluster_group_id, user_cluster_group_id, start_date, end_date, target, awareness):
 		median_step_list = []
 		single_date = start_date
+
+		all_streak_clusters = self.md_Streakinfo.objects.filter(group_id=streak_cluster_group_id)
+		all_user_clusters = self.md_Userclusterinfo.objects.filter(group_id=user_cluster_group_id)
+		print("user cluster id: " + str(user_cluster_group_id))
+
+		q_streak = self.dj_Q(pk=None)
+		q_user = self.dj_Q(pk=None)
+		# adding up all individuals under the group
+		for streak_cluster in all_streak_clusters:
+			print("streak cluster id: " + str(streak_cluster.id))
+			q_streak = q_streak | self.dj_Q(streak_cluster_id=streak_cluster.id)
+		for user_cluster in all_user_clusters:
+			print("user cluster id: " + str(user_cluster.id))
+			q_user = q_streak | self.dj_Q(user_cluster_id=user_cluster.id)
+
 		while single_date <= end_date:
-			all_streaks = self.md_Streak.objects.filter(streak_cluster_id=streak_id, user_cluster_id=group_id, cohort_day=single_date).order_by('step_count')
+			if target == "group" and awareness == "streak":
+				all_streaks = self.md_Streak.objects.filter(q_streak, q_user, cohort_day=single_date).order_by('step_count')
+			elif target == "group" and awareness != "streak":
+				all_streaks = self.md_Streak.objects.filter(q_user, cohort_day=single_date).order_by('step_count')
+			elif target == "all" and awareness == "streak":
+				all_streaks = self.md_Streak.objects.filter(q_streak, cohort_day=single_date).order_by('step_count')
+			else:
+				all_streaks = self.md_Streak.objects.filter(cohort_day=single_date).order_by('step_count')
+
 			count = all_streaks.count()
 			median = all_streaks[int(self.mt_math.floor(count/2))]
-			median_step_list.append(median)
+			median_step_list.append(median.step_count)
 			single_date = single_date + 1
 		return median_step_list
 
@@ -83,7 +114,14 @@ class MessageSender:
 		user = streak.user
 		all_streaks = self.md_Streak.objects.filter(user=user, streak_index=streak_index).order_by('calendar_date')
 		start = all_streaks[0]
-		return start
+		return start.calendar_date
+
+	def get_streak_start_date_cohort(self, streak):
+		streak_index = streak.streak_index
+		user = streak.user
+		all_streaks = self.md_Streak.objects.filter(user=user, streak_index=streak_index).order_by('calendar_date')
+		start = all_streaks[0]
+		return start.cohort_day
 
 	def generate_survey(self, section_type):
 		generated_survey = []
@@ -100,21 +138,21 @@ class MessageSender:
 				generated_survey.append({'index': 0, 'type': question.category, 'question': question.content, 'isAnswered': False, 'answer':"", 'options':generated_options})
 
 	def fetch_all_steps(self, user, start_date, end_date):
-		day = datetime.timedelta(days=1)
+		day = self.dt_datetime.timedelta(days=1)
 		single_date = start_date
 		step_list = []
 		while single_date <= end_date:
-			stepcount = self.md_Stepcount.objects.get(user=user, date=single_date)
+			stepcount = self.md_Stepcount.objects.get(user=user, date=single_date).step_count
 			step_list.append(stepcount)
 			single_date = single_date + day
 		return step_list
 
 	def fetch_all_dates(self, start_date, end_date):
-		day = datetime.timedelta(days=1)
+		day = self.dt_datetime.timedelta(days=1)
 		single_date = start_date
 		date_list = []
 		while single_date <= end_date:
-			date_list.append(single_date)
+			date_list.append(single_date.isoformat())
 			single_date = single_date + day
 		return date_list
 
@@ -122,24 +160,28 @@ class MessageSender:
 		generated_survey = self.generate_survey("streak_comparison")
 		
 		streak_cluster_id = streak.streak_cluster_id
-		streak_info = self.md_Streakinfo.filter(id=streak_id)
+		streak_info = self.md_Streakinfo.objects.get(id=streak_cluster_id)
 		streak_group_info = streak_info.group
 		streak_group_name = streak_group_info.name
 		streak_group_description = streak_group_info.description
 		streak_extra_description = streak_info.description
 
 		recommend_streak_group_id = streak_group_info.recommendation_id_ongoing #to check status!!
-		recommend_streak_group_info = self.md_Streakgroupinfo.filter(id=recommend_streak_group_id)
+		recommend_streak_group_info = self.md_Streakgroupinfo.objects.get(id=recommend_streak_group_id)
 		recommend_streak_group_name = recommend_streak_group_info.name
 		recommend_streak_group_description = recommend_streak_group_info.description
 
 		user_cluster_id = streak.user_cluster_id
-		user_cluster_info = self.md_Userclusterinfo.filter(id=user_cluster_id) #yet to do in db
+		user_cluster_info = self.md_Userclusterinfo.objects.get(id=user_cluster_id) #yet to do in db
 		user_cluster_group_info = user_cluster_info.group
+		user_cluster_group_id = user_cluster_group_info.id
 		user_cluster_group_name = user_cluster_group_info.name
 
 		start_date = self.get_streak_start_date(streak)
 		end_date = streak.calendar_date
+
+		cohort_start_date = self.get_streak_start_date_cohort(streak)
+		cohort_end_date = streak.cohort_day
 
 		calendar_steps = []
 		cohort_steps = []
@@ -148,8 +190,8 @@ class MessageSender:
 		all_steps = self.fetch_all_steps(user, start_date, end_date)
 		all_dates = self.fetch_all_dates(start_date, end_date)
 
-		calendar_steps = generate_comparison_calendar(recommend_streak_group_id, user_cluster_id, start_date, end_date, comparison_target, awareness)
-		cohort_steps = generate_comparison_cohort(recommend_streak_group_id, user_cluster_id, start_date, comparison_target, awareness)
+		calendar_steps = self.generate_comparison_calendar(recommend_streak_group_id, user_cluster_group_id, start_date, end_date, comparison_target, awareness)
+		cohort_steps = self.generate_comparison_cohort(recommend_streak_group_id, user_cluster_group_id, cohort_start_date, cohort_end_date, comparison_target, awareness)
 
 		return {
 					'type' : "streak-comparison",
@@ -160,8 +202,8 @@ class MessageSender:
 					'recommendStreakName' : recommend_streak_group_name,
 					'recommendStreakDescription' : recommend_streak_group_description,
 					'userClusterName' : user_cluster_group_name,
-					'startDate' : start_date,
-					'endDate' : end_date,
+					'startDate' : start_date.isoformat(),
+					'endDate' : end_date.isoformat(),
 					'steps' : all_steps,
 					'dates': all_dates,
 					'calendarSteps' : calendar_steps,
@@ -169,43 +211,11 @@ class MessageSender:
 					'survey' : generated_survey
 				}
 
-'''
-	def generate_section_streak_comparison_hard(self):
-		generated_survey = []
-		#wrong
-		survey = self.md_Question.objects.filter(section="streak_comparison")
-		for question in survey:
-			if not question.category=="mcq":
-				generated_survey.append({'index': 0, 'type': question.category, 'question': question.content, 'isAnswered': False, 'answer':""})
-			else:
-				generated_options = []
-				options = self.md_Option.objects.filter(question=question)
-				for option in options:
-					generated_options.append(option.content)
-				generated_survey.append({'index': 0, 'type': question.category, 'question': question.content, 'isAnswered': False, 'answer':"", 'options':generated_options})
-		return {
-					'type' : "streak-comparison",
-					'hasFinishedSurvey' : False,
-					'streakID' : 12,
-					'recommendID' : 15,
-					'percentile' : 75,
-					'startTime' : "2018-02-11",
-					'endTime' : "2018-02-15",
-					'steps' : [7502, 6702, 9520, 8640, 10020, 2540, 5203, 8201, 2005],
-					'dates': [11, 12, 13, 14, 15, 16, 17, 18, 19],
-					'calendarSteps' : [8200, 8702, 10200, 8040, 10070, 5420, 6802, 10020, 9520],
-					'cohortSteps' : [7660, 8402, 9200, 10040, 9070, 5360, 8204, 9204, 7200],
-					'streakDescription' : "You are walking slowly",
-					'recommendDescription' : "Try walking faster",
-					'survey' : generated_survey
-				}
-	'''
-	
-	def generate_section_stats_comparison(self, streak, comparison_target):
+	def generate_section_stats_comparison(self, streak, comparison_target, awareness):
 		generated_survey = self.generate_survey("stats_comparison")
 
 		user_cluster_id = streak.user_cluster_id
-		user_cluster_info = self.md_Userclusterinfo.filter(id=user_cluster_id) #yet to do in db
+		user_cluster_info = self.md_Userclusterinfo.objects.get(id=user_cluster_id) #yet to do in db
 		user_cluster_group_info = user_cluster_info.group
 		user_cluster_group_name = user_cluster_group_info.name
 
@@ -230,9 +240,9 @@ class MessageSender:
 				}
 
 	#todo: check control grp to pick the right comparison
-	def generate_section_comparison(self):
-		section_streak_comparison = self.generate_section_streak_comparison()
-		section_stats_comparison = self.generate_section_stats_comparison()
+	def generate_section_comparison(self, streak, comparison_target, awareness):
+		section_streak_comparison = self.generate_section_streak_comparison(streak, comparison_target, awareness)
+		#section_stats_comparison = self.generate_section_stats_comparison(streak, comparison_target, awareness)
 		return section_streak_comparison
 
 	def generate_section_prev_challenge(self):
@@ -358,15 +368,15 @@ class MessageSender:
 			msgref = self.fb_db.reference('profile/'+key+'/messages')
 			print("inside a user")
 			#section_graph = self.generate_section_graph()
-			streak = self.md_Streak.objects.get(user_id="1000242", date=self.dt_date(2015,11,21)) #122
-			section_comparison = self.generate_section_comparison()
+			streak = self.md_Streak.objects.get(user_id="8", calendar_date=self.dt_date(2015,11,22).isoformat()) #122
+			section_comparison = self.generate_section_comparison(streak, "group", "streak")
 			section_challenge = self.generate_section_challenge()
 			#section_prev_challenge = self.generate_section_prev_challenge()
 			
 			new_msg_ref = msgref.push()
 			new_msg_ref.set({
 				'content': "hehahah",
-				'time': {'date': 2, 'month': 3, 'year': 2018},
+				'time': {'date': 8, 'month': 4, 'year': 2018},
 				'hasCompleted': False,
 				'sections': [section_comparison, section_challenge]
 			})
@@ -378,7 +388,7 @@ class MessageSender:
 		#date=date.today(), defaults={'user_id': key, 'message_id': key})
 	#print stepcount
 	
-ms = MessageSender(db, Question, Option, Stepcount, Streak, date, datetime, math)
+ms = MessageSender(db, Q, Question, Option, Stepcount, Streak, StreakInfo, StreakGroupInfo, UserClusterInfo, UserClusterGroupInfo, date, datetime, math)
 ms.send_messages()
 
 start_date = date.today()
